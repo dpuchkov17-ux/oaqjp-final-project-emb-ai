1f34566fef085d007f27b4416df6b9fdad675a35
Dmitry Puchkov <dpuchkov17@gmail.com>
2026-09-14 13:45:17 -0400

    Update server route for emotion detector

--- server.py
+++ server.py
@@ -1,4 +1,29 @@
 from flask import Flask, render_template, request 
-from EmotionDetection.emotion_detection import test_emotion_detector
+from EmotionDetection.emotion_detection import emotion_detector
 
-app = Flask("Sentiment Analyzer")
+app = Flask("Emotion Detector")
+
+@app.route("/")
+def render_index_page():
+    return render_template('index.html')
+    
+@app.route("/emotionDetector")
+def emotion_analyzer():
+    text_to_analyze = request.args.get('textToAnalyze')
+    response = emotion_detector(text_to_analyze)
+
+    return (
+        "For the given statement, the system response is "
+        "'anger': {}, 'disgust': {}, 'fear': {}, 'joy': {} and "
+        "'sadness': {}. The dominant emotion is <b>{}<b>.".format(
+            response['anger'],
+            response['disgust'],
+            response['fear'],
+            response['joy'],
+            response['sadness'],
+            response['dominant_emotion']
+        )
+    )
+
+if __name__ == "__main__":
+      app.run(host="0.0.0.0", port=5003)
